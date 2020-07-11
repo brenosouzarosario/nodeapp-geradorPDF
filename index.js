042b5b5aa5f248ccad93c6f7607e3519a9613dc5
Breno Souza <brenosouza.rosario@gmail.com>
2020-07-11 18:21:14 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// var pdf = require("html-pdf");
+// var ejs = require("ejs");
+
+// var nomeDoUsuario = "Teste b";
+
+// ejs.renderFile("./index.ejs", { nome: nomeDoUsuario }, (err, html) => {
+//     if (err) {
+//         console.log("Erro");
+//     } else {
+//         pdf.create(html, {}).toFile("./meupdf.pdf", (err, res) => {
+//             if (err) {
+//                 console.log("Erro");
+//             } else {
+//                 console.log(res);
+//             }
+//         });
+//     }
+// });
+
+// var conteudo = `
+//     <h1>ada </h1>
+//     <hr>
+//     <p>asd </p>
+//     <p>${nomeDoUsuario}</p>
+// `;
