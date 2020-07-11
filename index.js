3561290d7035e0d628c825cb77e3e9a47adc0235
brenosouzarosario <54741934+brenosouzarosario@users.noreply.github.com>
2020-07-11 19:32:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-// var pdf = require("html-pdf");
-// var ejs = require("ejs");
+var pdf = require("html-pdf");
+var ejs = require("ejs");
 
-// var nomeDoUsuario = "Teste b";
+var nomeDoUsuario = "Teste b";
 
-// ejs.renderFile("./index.ejs", { nome: nomeDoUsuario }, (err, html) => {
-//     if (err) {
-//         console.log("Erro");
-//     } else {
-//         pdf.create(html, {}).toFile("./meupdf.pdf", (err, res) => {
-//             if (err) {
-//                 console.log("Erro");
-//             } else {
-//                 console.log(res);
-//             }
-//         });
-//     }
-// });
+ejs.renderFile("./index.ejs", { nome: nomeDoUsuario }, (err, html) => {
+    if (err) {
+        console.log("Erro");
+    } else {
+        pdf.create(html, {}).toFile("./meupdf.pdf", (err, res) => {
+            if (err) {
+                console.log("Erro");
+            } else {
+                console.log(res);
+            }
+        });
+    }
+});
 
-// var conteudo = `
-//     <h1>ada </h1>
-//     <hr>
-//     <p>asd </p>
-//     <p>${nomeDoUsuario}</p>
-// `;
+var conteudo = `
+    <h1>ada </h1>
+    <hr>
+    <p>asd </p>
+    <p>${nomeDoUsuario}</p>
+`;
